b5c72fc2f2d0361e68328f7c13ca9d6ba5e741eb
Kenneth Kebaara <kkebaara@yahoo.com>
2022-07-26 05:43:46 -0600

    added seeds to database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Review.destroy_all
+Author.destroy_all
+Book.destroy_all
+User.destroy_all
+
+
+puts "🌱 Seeding..."
+
+
+10.times do 
+	Author.create(first_name:Faker::Name.first_name, last_name:Faker::Name.last_name, website: Faker::Internet.domain_name, genres: Faker::Book.genre, description: Faker::TvShows::Seinfeld.quote)
+end
+
+10.times do 
+	Book.create(title: Faker::Book.title, description: Faker::TvShows::Seinfeld.quote, publish_date: Faker::Date.in_date_period, rating: rand(1..5), author: Author.all.sample)
+end
+
+10.times do 
+	User.create(first_name:Faker::Name.first_name, last_name:Faker::Name.last_name )
+end
+
+10.times do 
+	Review.create(rating:rand(1..5), description: Faker::TvShows::Seinfeld.quote, book: Book.all.sample, user: User.all.sample)
+end
+
+
+
+puts "✅ Done Seeding!"
